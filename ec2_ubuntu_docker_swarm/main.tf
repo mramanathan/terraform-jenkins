0ced8cd1744ef54e2a5c1f6f42dd85a316cd5f9a
Ramanathan.M <rus.cahimb@gmail.com>
2020-02-26 20:27:42 +0530

    cosmetic cleanup

--- ec2_ubuntu_docker_swarm/main.tf
+++ ec2_ubuntu_docker_swarm/main.tf
@@ -48,11 +48,12 @@ resource "aws_security_group" "allow_all" {
     # the Docker Swarm cluster.
     ingress {
         from_port = 0
-        to_port   = 65535
-        protocol  = "tcp"
+        to_port   = 0
+        protocol  = -1
         cidr_blocks = ["0.0.0.0/0"]
     }
 
+    # Redundant, but retained.
     # SSH connectivity
     ingress {
         from_port = 22
